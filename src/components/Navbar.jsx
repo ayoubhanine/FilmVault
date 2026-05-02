b24cf835c0d98f497075d2e41ddb223e84faf126
HANINE AYOUB <ayoubhanine2000@gmail.com>
2026-05-02 09:03:51 +0000

    styliser la button ajouter dun navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,8 @@ function Navbar({ onAddClick }) {
         <button
           onClick={onAddClick}
           className="bg-amber-300 text-black px-5 py-2 rounded-full font-semibold 
-          hover:bg-amber-400 hover:scale-105 transition duration-300 shadow-lg"
+          hover:bg-amber-400 hover:scale-105 transition duration-300 shadow-lg
+          cursor-pointer"
         >
           + Ajouter
         </button>
